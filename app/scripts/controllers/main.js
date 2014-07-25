6ef1df1a48bbd649e699bf36fa9b5c9142e5fc6c
Ramin B <i8ramin@gmail.com>
2014-07-24 21:29:52 -0400

    try again

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ruzbehkheirabiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
