0f8700a4950a3972d531b34337e8d02a63508f3b
Ramin B <i8ramin@gmail.com>
2014-07-27 17:24:41 -0400

    just about done

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,35 +1,41 @@
 'use strict';
 
 angular.module('ruzbehkheirabiApp').controller('MainCtrl', function ($scope, simpleLogin, CandleService) {
+  $scope.candleLit = false;
   $scope.entries = CandleService;
-  $scope.candle = {};
 
   $scope.lightCandle = function () {
-    $scope.candle.type = (Math.floor(Math.random() * 8) + 1);
-    $scope.candle.flame = (Math.floor(Math.random() * 5) + 1);
-
+    debugger;
     $scope.entries.$add({
-      from: $scope.auth.user,
-      private: $scope.isPrivate || false,
-      candle: $scope.candle
+      from: $scope.from,
+      candle: {
+        type: (Math.floor(Math.random() * 8) + 1),
+        flame: (Math.floor(Math.random() * 5) + 1)
+      },
+      note: $scope.note || ''
     });
 
-    $scope.candle = {};
+    $scope.note = '';
+    $scope.candleLit = true;
   };
 
-  $scope.isMine = function (entry) {
-    var user = $scope.auth ? $scope.auth.user : false;
-    // console.log(entry);
-    return user && entry.from && (entry.from.id === user.id);
+  $scope.lightAnother = function (e) {
+    e.preventDefault();
+    $scope.candleLit = false;
   };
 
-  $scope.hasPublicNote = function (entry) {
-    return entry.candle.note && !entry.private;
-  };
+  // $scope.isMine = function (entry) {
+  //   var user = $scope.auth ? $scope.auth.user : false;
+  //   return user && entry.from && (entry.from.id === user.id);
+  // };
 
-  $scope.shouldShowTooltip = function (entry) {
-    return (entry.candle.note && $scope.isMine(entry)) || $scope.hasPublicNote(entry)
-  };
+  // $scope.hasPublicNote = function (entry) {
+  //   return entry.note && !entry.private;
+  // };
+
+  // $scope.shouldShowTooltip = function (entry) {
+  //   return (entry.note && $scope.isMine(entry)) || $scope.hasPublicNote(entry);
+  // };
 
   $scope.login = function (e, service) {
     e.preventDefault();
