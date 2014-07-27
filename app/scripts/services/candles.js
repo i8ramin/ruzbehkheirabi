1e3c6b0009bdbb55fb6a01594891fac15e8265cc
Ramin B <i8ramin@gmail.com>
2014-07-27 17:26:57 -0400

    publish: just about done
    generated from commit 0f8700a4950a3972d531b34337e8d02a63508f3b

--- app/scripts/services/candles.js
+++
@@ -1,5 +0,0 @@
-angular.module('ruzbehkheirabiApp')
-  .factory('CandleService', ['$firebase', 'FBURL',  function($firebase, FBURL) {
-    var ref = new Firebase(FBURL + '/entries');
-    return $firebase(ref);
-  }]);
